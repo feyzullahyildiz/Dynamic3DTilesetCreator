e83c98b80f4be1dd69ef629f8fd5a4b1e77658b6
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2020-09-28 18:17:26 +0300

    example kod yapıdı

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const path = require('path')
 const express = require('express')
 const app = express();
-const { Cartesian3, HeadingPitchRoll, Transforms, Matrix4, Cartographic } = require('cesium')
-
+const fs = require('fs')
+const { Cartographic } = require('cesium');
+const createB3dm = require('./lib/createB3dm')
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.get('/api/tileset.json', (req, res) => {
-    
-    const southWest = Cartographic.fromDegrees(28.804961,41.067761);
-    const northEast = Cartographic.fromDegrees(28.815170,41.071550);
-    console.log('southWest', southWest)
-    console.log('northEast', northEast)
+
+    const southWest = Cartographic.fromDegrees(28.804961, 41.067761);
+    const northEast = Cartographic.fromDegrees(28.815170, 41.071550);
+    const b3dmSouthWest = Cartographic.fromDegrees(28.8051, 41.068);
+    const b3dmNorthEast = Cartographic.fromDegrees(28.8090, 41.070);
+    // console.log('southWest', southWest)
+    // console.log('northEast', northEast)
     const tilesetJson = {
         "asset": {
             "version": "0.0",
@@ -24,10 +27,6 @@ app.get('/api/tileset.json', (req, res) => {
             "refine": "ADD",
             "boundingVolume": {
                 "region": [
-                    // 0.5028020666793648,
-                    // 0.7158926326772073,
-                    // 0.5039214457439004,
-                    // 0.7168435953563773,
                     southWest.longitude,
                     southWest.latitude,
                     northEast.longitude,
@@ -35,12 +34,39 @@ app.get('/api/tileset.json', (req, res) => {
                     0,
                     196.10000000000002
                 ]
-            }
+            },
+            children: [
+                {
+                    "boundingVolume": {
+                        "region": [
+                            b3dmSouthWest.longitude,
+                            b3dmSouthWest.latitude,
+                            b3dmNorthEast.longitude,
+                            b3dmNorthEast.latitude,
+                            0,
+                            160
+                        ]
+                    },
+                    content: { uri: 'data.b3dm' },
+                    geometricError: 175.78125,
+                    level: 12
+                }
+            ]
         }
     }
     res.json(tilesetJson);
 })
 
+app.get('/api/data.b3dm', (req, res) => {
+    res.set('content-type', 'application/octet-stream');
+    const arrowGltfBuffer = fs.readFileSync(path.resolve(__dirname, 'public', 'asset', 'arrow.glb'));
+    const resultBuffer = createB3dm({
+        glb: arrowGltfBuffer,
+    })
+    // console.log(header);
+    // const resultBuffer = Buffer.concat([header, arrowGltfBuffer])
+    res.send(resultBuffer)
+})
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('app started')
--- src/public/main.js
+++ src/public/main.js
@@ -15,3 +15,16 @@ tileset.debugColorizeTiles = true
 viewer.scene.primitives.add(tileset);
 viewer.zoomTo(tileset, new Cesium.HeadingPitchRange(0.0, -0.3, 0.0));
 
+
+viewer.imageryLayers._layers[0].show = false
+
+const modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
+    Cesium.Cartesian3.fromDegrees(28.8, 41.067761, 0.0)
+);
+const model = Cesium.Model.fromGltf({
+    url: 'asset/arrow.gltf',
+    modelMatrix : modelMatrix,
+    scale : 10.0
+})
+
+viewer.scene.primitives.add(model);
